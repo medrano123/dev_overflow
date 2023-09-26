a988d366ca0afb4f99c8e599d784ec122876face
Giovanni Medrano <medranoector@yahoo.com>
2023-09-26 14:39:33 -0500

    navbar and mobile navbar ui done

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -1,31 +1,35 @@
 "use client"
-import React, { createContext, useContext, useState, useEffect } from 'react';
+
+import React, { createContext, useContext, useState, useEffect } from "react"
 
 interface ThemeContextType {
-    mode: string;
-    setMode: (mode: string) => void;
+  mode: string;
+  setMode: (mode: string) => void;
 }
 
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
-export function ThemeProvider({ children }: { children: React.ReactNode}) {
-	const [mode, setMode] = useState('')
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+	const [mode, setMode] = useState('');
 
 	const handleThemeChange = () => {
-		if(mode === 'dark') {
-			setMode('light');
-			document.documentElement.classList.add('light');
-		} else {
+		if(
+			localStorage.theme === 'dark' || 
+      (!("theme" in localStorage) && 
+      window.matchMedia("(prefers-color-scheme: dark)").matches)
+		) {
 			setMode('dark');
 			document.documentElement.classList.add('dark');
+		} else {
+			setMode('light');
+			document.documentElement.classList.remove('dark');
 		}
 	}
 
 	useEffect(() => {
 		handleThemeChange();
 	}, [mode])
-    
-
+  
 	return (
 		<ThemeContext.Provider value={{ mode, setMode }}>
 			{children}
@@ -40,5 +44,5 @@ export function useTheme() {
 		throw new Error('useTheme must be used within a ThemeProvider')
 	}
 
-	return context
+	return context;
 }
