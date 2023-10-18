09fa19bada426b7d0ac2e8ed0aca2e2cba5468a9
Giovanni Medrano <medranoector@yahoo.com>
2023-10-18 15:12:01 -0500

    votes ui completed

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -10,6 +10,8 @@ import RenderTag from '@/components/shared/RenderTag'
 import { formatAndDivideNumber, getTimestamp } from '@/lib/utils';
 import AllAnswers from '@/components/shared/AllAnswers'
 import { getUserById } from '@/lib/actions/user.actions'
+import Votes from '@/components/shared/Votes';
+
 
 
 const page = async ({ params, searchParams }) => {
@@ -19,7 +21,7 @@ const page = async ({ params, searchParams }) => {
 	if(clerkId){
 		mongoUser = await getUserById({userId: clerkId});
 	}
-	
+
 	return (
 		<>
 			<div className="flex-start w-full flex-col">
@@ -38,7 +40,16 @@ const page = async ({ params, searchParams }) => {
 						</p>
 					</Link>
 					<div className="flex justify-end">
-                        VOTING
+						<Votes 
+							type="Question"
+							itemId={JSON.stringify(result._id)}
+							userId={JSON.stringify(mongoUser._id)}
+							upvotes={result.upvotes.length}
+							hasupVoted={result.upvotes.includes(mongoUser._id)}
+							downvotes={result.downvotes.length}
+							hasdownVoted={result.downvotes.includes(mongoUser._id)}
+							hasSaved={mongoUser?.saved.includes(result._id)}
+						/>
 					</div>
 				</div>
 				<h2 className="h2-semibold text-dark200_light900 mt-3.5 w-full text-left">
