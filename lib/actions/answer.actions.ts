4ddaab9ac41af803d80f3e406ae8ed5a332b8973
Giovanni Medrano <medranoector@yahoo.com>
2023-10-18 07:29:21 -0500

    create answers server actions created

---
+++ lib/actions/answer.actions.ts
@@ -0,0 +1,27 @@
+"use server"
+import { revalidatePath } from "next/cache";
+
+import { CreateAnswerParams } from "./shared.types";
+import { connectToDatabase } from "../mongoose";
+import Answer from "@/database/answer.model";
+import Question from "@/database/question.model";
+
+export async function createAnswer({ params }: CreateAnswerParams){
+	try {
+		connectToDatabase()
+		const { content, author, path, question } = params;
+		const newAnswer = await Answer.create({ content, author, question });
+
+		// Add the answer to the question's answers array
+		await Question.findByIdAndUpdate(question, {
+			$push: { answers: newAnswer._id}
+		})
+  
+		// TODO: Add interaction...
+
+		revalidatePath(path)
+	} catch (error) {
+		console.log(error);
+		throw error
+	}
+}
