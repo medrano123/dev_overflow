1e8aa02700a2055a6b6eceeabe3e32af1300150e
Giovanni Medrano <medranoector@yahoo.com>
2023-10-24 08:16:29 -0500

    stats section of profile page

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -70,7 +70,10 @@ const Page = async ({ params, searchParams }: URLProps) => {
 					</SignedIn>
 				</div>
 			</div>
-            stats
+			<Stats
+			        totalQuestions={userInfo.totalQuestions}
+				totalAnswers={userInfo.totalAnswers}
+			/>
 			<div className="mt-10 flex gap-10">
 				<Tabs defaultValue="top-posts" className="flex-1">
 					<TabsList className="background-light800_dark400 min-h-[42px] p-1">
