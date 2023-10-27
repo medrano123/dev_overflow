91561f53f2d3251278840e2e8042dbd0051af8e2
Giovanni Medrano <medranoector@yahoo.com>
2023-10-27 15:29:24 -0500

    top questions and tags

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -31,16 +31,17 @@ const Question = ({ type, mongoUserId, questionDetails }: Props) => {
 
 	const [isSubmitting, setIsSubmitting] = useState(false)
 	
-	const parsedQuestionDetails = JSON.parse(questionDetails || '{}');
-	const groupedTags = parsedQuestionDetails?.tags?.map((tag) => tag.name) || [];
+	const parsedQuestionDetails =  questionDetails && JSON.parse(questionDetails || '');
+
+	const groupedTags = parsedQuestionDetails?.tags.map((tag) => tag.name)
 
 
 	// 1. Define your form.
 	const form = useForm<z.infer<typeof QuestionsSchema>>({
 		resolver: zodResolver(QuestionsSchema),
 		defaultValues: {
-			title: parsedQuestionDetails.title || '',
-			explanation: parsedQuestionDetails.content || '',
+			title: parsedQuestionDetails?.title || '',
+			explanation: parsedQuestionDetails?.content || '',
 			tags: groupedTags || [],
 		},
 	})
@@ -146,7 +147,7 @@ const Question = ({ type, mongoUserId, questionDetails }: Props) => {
 									}}
 									onBlur={field.onBlur}
 									onEditorChange={(content) => field.onChange(content)}
-									initialValue={parsedQuestionDetails.content || ''}
+									initialValue={parsedQuestionDetails?.content || ''}
 									init={{
 										height: 350,
 										menubar: false,
